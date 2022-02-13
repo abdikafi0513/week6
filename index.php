2ca5b353afbc0e137e11ff23200360b631037020
Abdikafi Jama <jama.abdikafi@student.greenriver.edu>
2022-02-12 23:15:03 -0500

    new stuffs added

--- index.php
+++ index.php
@@ -4,10 +4,9 @@ ob_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
-var_dump($_SESSION);
 //Require the autoload file
 require_once('vendor/autoload.php');
+require('model/data_layer.php');
 
 
 
@@ -15,12 +14,31 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 
-//Define a default route
-$f3->route('GET /', function() {
-    //echo '<h1>My diner!</h1>';
-    $view = new Template();
-    echo $view->render('views/info.html');
 
 
-}
-);
+
+    $f3->route('GET /', function ($f3) {
+
+        //save variables to the F3 "hive"
+        $f3->set('username', 'jshmo');
+        $f3->set('password', sha1('Password01'));
+        $f3->set('title', 'Working with Templates');
+        $f3->set('temp', 67);
+        $f3->set('color', 'my favorite color is purpl');
+        $fruits=array('apple', 'orange','banana');
+        $f3->set('fruits', $fruits);
+
+        $f3-> set('cities',getcities());
+        $f3->set('colors', getcolors());
+
+
+
+        //display a template
+        $template = new Template();
+        echo $template->render('views/info.html');
+
+        //alternate syntax
+        echo Template::instance()->render('views/info.html');
+    });
+//Run fat free
+$f3->run();
---
+++ model/data_layer.php
@@ -0,0 +1,11 @@
+<?php
+
+function getcities(){
+    $pairs=array('Washington'=>'seattle','MN'=>'MINI','LA'=>'lusiana');
+    return $pairs;
+}
+function getcolors(){
+    $colors=array('red','white','black');
+    return $colors;
+}
+
